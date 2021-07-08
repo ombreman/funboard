5be8e4356f913c9075fb8322341fa7c48b09fe4b
Changgil Jeong <ombreman21@gmail.com>
2021-07-08 12:14:59 +0100

    add delete function

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use(express.urlencoded({ extended: true }));
 const MongoClient = require('mongodb').MongoClient;
 app.set('view engine', 'ejs');
 
-const { get } = require('mongoose'); // ???? 이거 언제적음???
+// const { get } = require('mongoose'); // ???? 이거 언제적음???
 
 var db;
 MongoClient.connect('mongodb+srv://jcg3417:1q2w3e4r@cluster0.8pzke.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', (error, client) => {
@@ -26,19 +26,30 @@ app.get('/write', (req, res) => {
 });
 
 app.post('/add', (req, res) => {
-    res.send('전송완료')
-    db.collection('counter').findOne({ name: 'postNumber' }, function(err, result){
+    db.collection('counter').findOne({ name: 'postNumber' }, (error, result) => {
         console.log(result.totalPost);
-        let numberOfPosts = result.totalPost
-
-        db.collection('post').insertOne({ _id: numberOfPosts, date: req.body.date, title: req.body.title }, (error, result) => {
+        let numberOfPost = result.totalPost;
+        db.collection('post').insertOne({ _id: numberOfPost + 1, date: req.body.date, title: req.body.title }, (error, result) => {
             console.log('저장완료');
+            db.collection('counter').updateOne({ name: 'postNumber' }, { $inc: { totalPost: 1 } }, (error, result) => {
+                if (error) {return console.log('에러입니다!')};
+                res.send('전송완료');
+            });
+        });
     });
 });
 
 app.get('/list', (req, res) => {
-    db.collection('post').find().toArray(function (error, result){
-        console.log(result);
+    db.collection('post').find().toArray((error, result) => {
         res.render('list.ejs', { posts: result });
+    });
+});
+
+app.delete('/delete', (req, res) => {
+    console.log(req.body)
+    req.body = parseInt(req.body._id);
+    db.collection('post').deleteOne(req.body, (error, result) => {
+        console.log('게시글 삭제완료')
+        req.status(200).send({ message: '얏호! 성공!' });
     });
 });
